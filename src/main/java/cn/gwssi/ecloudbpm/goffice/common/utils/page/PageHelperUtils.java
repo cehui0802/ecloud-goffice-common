92f33fcefeb89e4b9a1a5070b20cb2180028f311
weipengxiang <pengxiang.wei@mushiny.com>
2020-09-04 09:34:54 +0800

    新增PageHelper工具类

---
+++ src/main/java/cn/gwssi/ecloudbpm/goffice/common/utils/page/PageHelperUtils.java
@@ -0,0 +1,51 @@
+package cn.gwssi.ecloudbpm.goffice.common.utils.page;
+
+import cn.gwssi.ecloudbpm.goffice.common.model.PageQuery;
+import com.github.pagehelper.PageHelper;
+import org.springframework.util.StringUtils;
+
+/**
+ * Title: PageUtils<br>
+ * Company:GWSSI</br>
+ * CreateDate:2020/09/03 9:47
+ *
+ * @author Lyric1st
+ */
+
+
+public class PageHelperUtils {
+
+    /**
+     * Title: 开启分页和排序<br>
+     * Description: startPageAndOrderBy<br>
+     * CreateDate: 2020/9/3 11:14<br>
+     * @param pageQuery
+     * @param defaultOrder 传入""或者null 则不需要排序
+     * @return void
+     * @category 开启分页和排序
+     * @author Lyric1st
+     */
+    public static void startPageAndOrderBy(PageQuery pageQuery, String defaultOrder) {
+        if (!pageQuery.isNoPage()) {
+            // 前端传入需要分页
+            PageHelper.startPage(pageQuery.getPageNo(), pageQuery.getPageSize());
+        }
+        orderBy(pageQuery, defaultOrder);
+
+    }
+
+    private static void orderBy(PageQuery pageQuery, String defaultOrder) {
+        String orderBy = "";
+        if (!StringUtils.isEmpty(pageQuery.getOrderBy())) {
+            orderBy += pageQuery.getOrderBy();
+            // 判断前端传入排序规则
+            if (!StringUtils.isEmpty(pageQuery.getSort())) {
+                orderBy += " " + pageQuery.getSort();
+            }
+            PageHelper.orderBy(orderBy);
+        } else if (StringUtils.isEmpty(defaultOrder)) {
+            // 前端没有传排序规则 并且有默认排序规则
+            PageHelper.orderBy(defaultOrder);
+        }
+    }
+}
